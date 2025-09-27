0d6a6fc1380e172a27e39fc5bbbcb5bb3e150485
Kairat Abuov <you@example.com>
2025-09-27 12:42:24 +0500

    v 0.3

--- numbers-gui/main.gd
+++ numbers-gui/main.gd
@@ -1,15 +1,22 @@
 ﻿extends Control
 
-# Кэшируем ссылки на узлы интерфейса по именам из сцены
-@onready var input_field: LineEdit = $LineEdit
-@onready var result_label: Label = $Label
-@onready var submit_button: Button = $Button
+# Получаем ссылки на элементы интерфейса
+@onready var date_input: LineEdit = $DateLineEdit
+@onready var name_input: LineEdit = $NameLineEdit
+@onready var date_button: Button = $DateButton
+@onready var name_button: Button = $NameButton
+@onready var date_label = $DateLabel
+@onready var name_label = $NameLabel
 
 func _ready() -> void:
-    # Привязываем сигнал нажатия к обработчику после готовности сцены
-    submit_button.pressed.connect(_on_button_pressed)
+    # Подписываем кнопки на обработчики
+    date_button.pressed.connect(_on_date_button_pressed)
+    name_button.pressed.connect(_on_name_button_pressed)
 
-func _on_button_pressed() -> void:
-    # Берём текст из поля ввода и показываем его в Label
-    var user_text := input_field.text
-    result_label.text = "Result: " + user_text
+func _on_date_button_pressed() -> void:
+    # Показываем введенную дату
+    date_label.text = "Дата: " + date_input.text
+
+func _on_name_button_pressed() -> void:
+    # Показываем введённое ФИО
+    name_label.text = "ФИО: " + name_input.text
